2a6789019f8d846d88948b590b8f14bcc6ab8941
Quentin cornevin <qcornevin@gmail.com>
2015-10-19 17:07:40 +0200

    Interface

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/derp.java
+++
@@ -1,7 +0,0 @@
-package fr.unice.polytech.soa1.shop3000;
-
-/**
- * Created by Quentin on 10/19/2015.
- */
-public class derp {
-}
---
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/utils/Endpoints.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.soa1.shop3000.utils;
+
+/**
+ * Created by Quentin on 10/19/2015.
+ */
+public interface Endpoints {
+
+    String CLIENT_FILE_INPUT = "file:camel/input";
+}
