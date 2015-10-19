4e2e40ac98519220187b24c45219f7618f280389
Quentin cornevin <qcornevin@gmail.com>
2015-10-19 17:01:49 +0200

    Archi maven

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/derp.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/derp.java
@@ -1,3 +1,5 @@
+package fr.unice.polytech.soa1.shop3000;
+
 /**
  * Created by Quentin on 10/19/2015.
  */
---
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/CreateClientFile.java
@@ -0,0 +1,14 @@
+package fr.unice.polytech.soa1.shop3000.flows;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Created by Quentin on 10/19/2015.
+ */
+public class CreateClientFile extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
