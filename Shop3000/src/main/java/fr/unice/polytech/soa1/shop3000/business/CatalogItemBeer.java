77fc96b396f3bd76c148981e5e5369ac1b706c2e
LaureenG <laureen.ginier@gmail.com>
2015-10-26 18:43:54 +0100

    formattage des json des catalogues (mode hardcoder...)

---
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/business/CatalogItemBeer.java
@@ -0,0 +1,21 @@
+package fr.unice.polytech.soa1.shop3000.business;
+
+import fr.unice.polytech.soa1.shop3000.business.customization.Customization;
+
+/**
+ * @author Laureen Ginier
+ */
+public class CatalogItemBeer extends CatalogItem {
+    public CatalogItemBeer(String name, double price, String description, Customization customization) {
+        super(name, price, description, customization);
+    }
+
+    /*@JsonCreator
+    public CatalogItemBeer(@JsonProperty(value = "name", required = true) String name,
+                           @JsonProperty(value = "price", required = true) double price,
+                           @JsonProperty(value = "description", required = true) String description,
+                           @JsonProperty(value = "color", required = false) Customization customization) {
+        super(name, price, "", customization);
+    }*/
+
+}
--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/catalog/CallExternalPartners.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/catalog/CallExternalPartners.java
@@ -1,28 +1,37 @@
 package fr.unice.polytech.soa1.shop3000.flows.catalog;
 
+import fr.unice.polytech.soa1.shop3000.business.CatalogItem;
 import fr.unice.polytech.soa1.shop3000.utils.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 
 /**
  * Created by Quentin on 10/21/2015.
  */
 public class CallExternalPartners extends RouteBuilder {
 
-    private ReadResponseStream readResponseStream;
+    private ReadResponseStream readResponseStreamBiko;
+    private ReadResponseStream readResponseStreamVolley;
+    private ReadResponseStream readResponseStreamBeer;
+
 
     public CallExternalPartners() {
-        this.readResponseStream = new ReadResponseStream();
+        this.readResponseStreamBiko = new ReadResponseStream("biko");
+        this.readResponseStreamVolley = new ReadResponseStream("volleyonthebeach");
+        this.readResponseStreamBeer = new ReadResponseStream("allhailbeer");
     }
 
     @Override
     public void configure() throws Exception {
         from(Endpoint.BIKO_CATALOG.getInstruction())
                 .log("Begin processing : Get Biko catalog")
-                .setHeader(Exchange.HTTP_METHOD,constant("GET"))
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(constant(""))
                 .to("http://localhost:8181/cxf/biko/catalog?bridgeEndpoint=true")
-                .process(readResponseStream)
+                .setProperty("shopName", constant("biko"))
+                //.unmarshal().json(JsonLibrary.Jackson,CatalogItem.class)
+                .process(readResponseStreamBiko)
                 .log("${body}");
 
         from(Endpoint.VOLLEY_CATALOG.getInstruction())
@@ -30,7 +39,9 @@ public class CallExternalPartners extends RouteBuilder {
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(constant(""))
                 .to("http://localhost:8181/cxf/volley/catalog?bridgeEndpoint=true")
-                .process(readResponseStream)
+                .setProperty("shopName", constant("volleyonthebeach"))
+                //.unmarshal().json(JsonLibrary.Jackson, CatalogItem.class)
+                .process(readResponseStreamVolley)
                 .log("${body}");
 
         from(Endpoint.BEER_CATALOG.getInstruction())
@@ -38,7 +49,9 @@ public class CallExternalPartners extends RouteBuilder {
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(constant(""))
                 .to("http://localhost:8181/cxf/shop/beers/all?bridgeEndpoint=true")
-                .process(readResponseStream)
+                .setProperty("shopName", constant("allhailbeer"))
+                //.unmarshal().json(JsonLibrary.Jackson, CatalogItem.class)
+                .process(readResponseStreamBeer)
                 .log("${body}");
     }
 }
