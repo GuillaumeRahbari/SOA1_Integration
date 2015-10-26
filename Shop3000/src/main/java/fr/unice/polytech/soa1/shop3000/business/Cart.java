a93bd7199db8ceb21e20f072cdeb0f0c3a733741
TomVeniat <veniat.tom@gmail.com>
2015-10-26 17:34:57 +0100

    Change to Cart model

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/business/Cart.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/business/Cart.java
@@ -1,28 +1,12 @@
 package fr.unice.polytech.soa1.shop3000.business;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by Quentin on 10/25/2015.
+ *
  */
-public class Cart {
+public class Cart extends HashMap<String, List<CatalogItem>> {
 
-    private List<CatalogItem> catalogItemList;
-
-    public Cart(List<CatalogItem> catalogItems) {
-        this.catalogItemList = catalogItems;
-    }
-
-    public Cart() {
-        this.catalogItemList = new ArrayList<CatalogItem>();
-    }
-
-    public List<CatalogItem> getCatalogItemList() {
-        return catalogItemList;
-    }
-
-    public void setCatalogItemList(List<CatalogItem> catalogItemList) {
-        this.catalogItemList = catalogItemList;
-    }
 }
--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/AddItemToCart.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/AddItemToCart.java
@@ -5,6 +5,8 @@ import fr.unice.polytech.soa1.shop3000.business.Client;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+import java.util.ArrayList;
+
 /**
  * Created by Quentin on 10/25/2015.
  */
@@ -12,9 +14,22 @@ public class AddItemToCart implements Processor {
 
 
     public void process(Exchange exchange) throws Exception {
-        Client client = exchange.getIn().getBody(Client.class);
-        CatalogItem catalogItem = exchange.getIn().getBody(CatalogItem.class);
 
-        client.getCart().getCatalogItemList().add(catalogItem);
+        Client client = exchange.getIn().getBody(Client.class);
+        if (client != null) {
+            System.out.println("Got a client : " + client);
+        }else System.out.println("No client");
+        CatalogItem catalogItem = exchange.getIn().getBody(CatalogItem.class);
+        if (catalogItem != null) {
+            System.out.printf("Got a catalogItem : " + catalogItem);
+        }else System.out.println("No catalogItem");
+
+
+        String shop = "ShopTest";//exchange.getIn();
+
+        if (client.getCart().get(shop)==null) {
+            client.getCart().put(shop, new ArrayList<CatalogItem>());
+        }
+        client.getCart().get(shop).add(catalogItem);
     }
 }
