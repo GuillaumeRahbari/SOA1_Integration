f40a816b7b9dadfe47722b12a06eb3f9e121f4a7
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-10-26 17:49:13 +0100

    Stash de merde

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/CartFlows.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/CartFlows.java
@@ -2,6 +2,7 @@ package fr.unice.polytech.soa1.shop3000.flows.cart;
 
 import fr.unice.polytech.soa1.shop3000.flows.catalog.ReadResponseStream;
 import fr.unice.polytech.soa1.shop3000.flows.clientfile.AddClientToDataBase;
+import fr.unice.polytech.soa1.shop3000.flows.clientfile.CheckClientInDatabase;
 import fr.unice.polytech.soa1.shop3000.mock.ItemMock;
 import fr.unice.polytech.soa1.shop3000.utils.Endpoint;
 import org.apache.camel.Exchange;
@@ -16,6 +17,7 @@ public class CartFlows extends RouteBuilder {
     private AddItemToCart addItemToCart;
     private ReadResponseStream readResponseStream;
     private CheckClientExistence checkClientExistence;
+    private CheckClientInDatabase checkClientInDatabase;
 
     public CartFlows() {
         this.itemMock = new ItemMock();
@@ -29,21 +31,25 @@ public class CartFlows extends RouteBuilder {
             from(Endpoint.ADD_ITEM_CART.getInstruction())
                     .log("Begin add item to cart")
                     .process(itemMock)
+                    .process(checkClientInDatabase)
+                    .choice()
+                        .when(simple("${body.firstName} == null && ${body.lastName} == null"))
+                            .log("Client already exist")
+                        .otherwise()
+                            .log("The client doesn't exist")
+                            .log("Begin process to add the client in the database")
+                            .log("Client added to the database")
                     .log("Client and item mocked in the exchange")
                     .process(addItemToCart)
                     .log("Item added to cart");
 
 
+        from(Endpoint.VALIDATE_CART.getInstruction())
+                .log("Begin validate cart")
+                .choice()
+                .when();
 
-
-
-
-            from(Endpoint.VALIDATE_CART.getInstruction())
-                    .log("Begin validate cart")
-                    .choice()
-                        .when();
-
-            from(Endpoint.CHECK_CLIENT_BEER.getInstruction())
+        from(Endpoint.CHECK_CLIENT_BEER.getInstruction())
                     .log("Begin check client")
                     .process(checkClientExistence)
                     .choice()
@@ -52,17 +58,17 @@ public class CartFlows extends RouteBuilder {
                         .when(simple("${header.result} == false"))
                             .to(Endpoint.ADD_TO_CART_ALL_HAIL_BEER.getInstruction());
 
-            from(Endpoint.CHECK_CLIENT_BIKO.getInstruction())
-                    .log("Begin check client")
-                    .setHeader(Exchange.HTTP_METHOD,constant("GET"))
-                    .setBody(constant(""))
-                    .to("http://localhost:8181/cxf/biko/clients/name/user1?bridgeEndpoint=true")
-                    .process(checkClientExistence)
-                    .choice()
-                        .when(simple("${header.result} == true"))
-                            .to(Endpoint.ADD_ITEM_CART.getInstruction())
-                        .when(simple("${header.result} == false"))
-                            .to(Endpoint.ADD_TO_CART_BIKO.getInstruction());
+        from(Endpoint.CHECK_CLIENT_BIKO.getInstruction())
+                .log("Begin check client")
+                .setHeader(Exchange.HTTP_METHOD,constant("GET"))
+                .setBody(constant(""))
+                .to("http://localhost:8181/cxf/biko/clients/name/user1?bridgeEndpoint=true")
+                .process(checkClientExistence)
+                .choice()
+                .when(simple("${header.result} == true"))
+                .to(Endpoint.ADD_ITEM_CART.getInstruction())
+                .when(simple("${header.result} == false"))
+                .to(Endpoint.ADD_TO_CART_BIKO.getInstruction());
 
             from(Endpoint.CHECK_CLIENT_VOLLEY.getInstruction())
                     .log("Begin check client")
