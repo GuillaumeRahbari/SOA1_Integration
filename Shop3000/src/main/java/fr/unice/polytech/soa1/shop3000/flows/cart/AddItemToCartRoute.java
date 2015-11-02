61bdc5674a4b16d7cdad5efcfb68a6698a64e665
Quentin cornevin <qcornevin@gmail.com>
2015-11-02 17:39:08 +0100

    Refractor

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/AddItemToCartRoute.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/AddItemToCartRoute.java
@@ -23,10 +23,5 @@ public class AddItemToCartRoute extends RouteBuilder {
         from(Endpoint.CHECK_REQUEST_STATUS.getInstruction())
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("${property.status}"));
 
-
-        // Expose the cart of a given client
-        rest("{clientID}/cart")
-                .get()
-                .to(Endpoint.GET_CART.getInstruction());
     }
 }
