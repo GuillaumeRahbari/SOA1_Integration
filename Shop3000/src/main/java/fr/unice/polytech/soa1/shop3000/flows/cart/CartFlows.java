a30dbc11bf07f803ad0f1ae8e62494b9633171ce
Nabil ELMOUSSAID <elmoussaid.nabil@gmail.com>
2015-11-02 17:12:12 +0100

    Debut de flow + process pour creation client biko

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/CartFlows.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/CartFlows.java
@@ -18,6 +18,7 @@ public class CartFlows extends RouteBuilder {
     private CheckClientExistenceVolley checkClientExistenceVolley;
     private CheckClientInDatabase checkClientInDatabase;
     private JsonToItem jsonToItem;
+    private CreateClientBiko createClientBiko;
 
     public CartFlows() {
         this.itemMock = new ItemMock();
@@ -27,6 +28,7 @@ public class CartFlows extends RouteBuilder {
         this.checkClientExistenceVolley = new CheckClientExistenceVolley();
         this.checkClientInDatabase = new CheckClientInDatabase();
         this.jsonToItem = new JsonToItem();
+        this.createClientBiko = new CreateClientBiko();
     }
 
 
@@ -66,14 +68,13 @@ public class CartFlows extends RouteBuilder {
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(constant(""))
                 .setProperty("login",constant("test"))
-                .setProperty("password",constant("test"))
+                .setProperty("password", constant("test"))
                 .recipientList(simple("http://localhost:8181/cxf/shop/account/${property.login}/${property.password}?bridgeEndpoint=true"))
                 .process(checkClientExistenceBeer)
                 .choice()
                     .when(simple("${property.result} == true"))
-                        .to(Endpoint.ADD_ITEM_CART.getInstruction())
                     .when(simple("${property.result} == false"))
-                        .to(Endpoint.ADD_TO_CART_ALL_HAIL_BEER.getInstruction());
+                        .to(Endpoint.CREATE_CLIENT_ALL_HAIL_BEER.getInstruction());
 
         /**
          * This flow check if the client is in the biko system, if not we create the client in the system and then
@@ -81,16 +82,15 @@ public class CartFlows extends RouteBuilder {
          */
         from(Endpoint.CHECK_CLIENT_BIKO.getInstruction())
                 .log("Begin check client")
-                .setHeader(Exchange.HTTP_METHOD,constant("GET"))
+                .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(constant(""))
                 .setProperty("clientID", constant("user1"))
                 .recipientList(simple("http://localhost:8181/cxf/biko/clients/name/${property.clientID}?bridgeEndpoint=true"))
                 .process(checkClientExistenceBiko)
                 .choice()
                     .when(simple("${property.result} == true"))
-                        .to(Endpoint.ADD_ITEM_CART.getInstruction())
                     .when(simple("${property.result} == false"))
-                        .to(Endpoint.ADD_TO_CART_BIKO.getInstruction());
+                        .to(Endpoint.CREATE_CLIENT_BIKO.getInstruction());
 
         /**
          * This flow check if client is already registered in the volley system. Create the client otherwise.
@@ -100,15 +100,25 @@ public class CartFlows extends RouteBuilder {
                 .log("Begin check client")
                 .setHeader(Exchange.HTTP_METHOD, constant("GET"))
                 .setBody(constant(""))
-                .setProperty("login",constant("jean"))
+                .setProperty("login", constant("jean"))
                 .recipientList(simple("http://localhost:8181/cxf/volley/accounts/${property.login}?bridgeEndpoint=true"))
                 .process(checkClientExistenceVolley)
                 .choice()
                     .when(simple("${property.result} == true"))
-                        .to(Endpoint.ADD_ITEM_CART.getInstruction())
                     .when(simple("${property.result} == false"))
-                        .to(Endpoint.ADD_TO_CART_VOLLEY_ON_THE_BEACH.getInstruction());
-        }
+                        .to(Endpoint.CREATE_CLIENT_VOLLEY_ON_THE_BEACH.getInstruction());
+
+
+        from(Endpoint.CREATE_CLIENT_BIKO.getInstruction())
+                .log("Begin create biko client")
+                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+                .setBody(constant(""))
+                .setProperty("username",constant("test"))
+                .process(createClientBiko)
+                .recipientList(simple("http://localhost:8181/cxf/biko/clients?bridgeEndpoint=true"));
+
+
+    }
 
 
 }
