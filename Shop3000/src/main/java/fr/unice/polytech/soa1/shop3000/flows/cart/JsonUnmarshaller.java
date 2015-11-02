39111c835fbed727fd0667ff9a6f0d0f9e02628f
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-11-02 17:27:41 +0100

    documentation jsonUnmarshaller

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/JsonUnmarshaller.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/JsonUnmarshaller.java
@@ -12,11 +12,19 @@ import org.apache.camel.Processor;
 public class JsonUnmarshaller extends SuperProcessor {
 
 
+    /**
+     * This process unmarshalls the json body and creates a property item with the object.
+     * @param exchange This Exchange object has a body containing a CatalogItem.
+     * @throws Exception
+     */
     @Override
     public void process(Exchange exchange) throws Exception {
-        String body = extractBodyFromExchange(exchange);
+        // Read the exchange body.
+        String body = extractExchangeBody(exchange);
+        // Mapping of the body into the object CatalogItem.
         ObjectMapper mapper = new ObjectMapper();
         CatalogItem catalogItem = mapper.readValue(body, CatalogItem.class);
+        // Set of the item property with the CatalogItem object
         exchange.setProperty("item", catalogItem);
     }
 }
