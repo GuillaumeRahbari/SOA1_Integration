19d85c43f4c691bb13255e4b688c548f634d75c2
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-11-02 17:15:29 +0100

    Documentation addItem

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/AddItem.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/AddItem.java
@@ -18,17 +18,33 @@ import java.util.List;
  */
 public class AddItem {
 
-    public boolean addItemToCart(String clientFirstName, CatalogItem catalogItem) throws IOException {
+    /**
+     * This function is the bean that we call to add an item to a cart.
+     *
+     * @param clientFirstName A string containing the client first name.
+     * @param catalogItem A CatalogItem containing the items we want to add.
+     * @return A boolean. It returns :
+     * <ul>
+     *     <li>True when the client exists. In this case, it also adds the items in client cart.</li>
+     *     <li>False when the client doesn't exist.</li>
+     * </ul>
+     */
+    public boolean addItemToCart(String clientFirstName, CatalogItem catalogItem){
         Client client = ClientStorage.read(clientFirstName);
         // If the client exists.
         if (client != null){
-            // We map the object to be a json object.
             addItemToCart(client,catalogItem);
             return true;
         };
         return false;
     }
 
+    /**
+     * This function is really the one adding a CatalogItem into the client cart.
+     * 
+     * @param client The client we want to add items in his cart.
+     * @param catalogItem The items we want to add in the client cart.
+     */
     private void addItemToCart (Client client, CatalogItem catalogItem) {
         String shopName = catalogItem.getName();
         if (client.getCart().get(shopName) == null) {
