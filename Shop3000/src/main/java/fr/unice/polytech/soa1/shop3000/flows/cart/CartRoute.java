a5c0913a0aa6e0a9b0434ae7c0d92fe3f28127ea
Quentin cornevin <qcornevin@gmail.com>
2015-11-06 16:57:18 +0100

    derp je comprend plus rien au code

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/CartRoute.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/cart/CartRoute.java
@@ -14,21 +14,28 @@ public class CartRoute extends RouteBuilder {
         restConfiguration().component("servlet");
 
         /**
+         * Begin of the flow to add an item to the cart of the client
+         *
          * {@link Unmarshaller}
          */
         rest("{clientID}/cart")
                 .put()
                 .to(Endpoint.UNMARSHALL_JSON_ITEM.getInstruction());
 
+        /**
+         * End of the flow started by : PUT /{clientID]/cart
+         * Comes from {@link CartFlows}
+         */
+        from(Endpoint.CHECK_REQUEST_STATUS.getInstruction())
+                .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("${property.status}"));
+
+
+        /**
+         * Juste du test : va disparaitre
+         */
         rest("/clients/createBiko")
                 .post()
                 .to(Endpoint.CREATE_CLIENT_BIKO.getInstruction());
-
-        
-        // Defining what we do after we checked the status. We set the header with the good status.
-
-        from(Endpoint.CHECK_REQUEST_STATUS.getInstruction())
-                .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("${property.status}"));
 
     }
 }
