f7d086bfbd8508766b9bab16b666bf7be942b8a0
Marc Karassev <marc.karassev@greencom-networks.com>
2015-11-07 13:15:38 +0100

    comments

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/PayRoute.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/PayRoute.java
@@ -32,6 +32,7 @@ public class PayRoute extends RouteBuilder {
             .process(jsonPaymentInformationExtractor)
             .setProperty(CLIENT_ID_PROPERTY, constant("${header.clientID}"))
             .log("client: ${" + CLIENT_ID_PROPERTY + "}")
+            // {@link ValidateCartAndPayment#configure() next} flow
             .to(Endpoint.VALIDATE_CART.getInstruction());
     }
 }
--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/ValidateCartAndPayment.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/ValidateCartAndPayment.java
@@ -1,16 +1,21 @@
 package fr.unice.polytech.soa1.shop3000.flows.pay;
 
-import fr.unice.polytech.soa1.shop3000.flows.BooleanAndAggregationStrategy;
 import fr.unice.polytech.soa1.shop3000.utils.Endpoint;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
  * @author Marc Karassev
  */
-public class ValidateCart extends RouteBuilder {
+public class ValidateCartAndPayment extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+
+        /** Flow checking payment information.
+         *  Expects a property "paymentInformation" with a JSON representing a PaymentInformation object.
+         */
+        // TODO
+
         from(Endpoint.VALIDATE_CART.getInstruction())
                 .log("starting cart validation")
                 .log("body: ${body}")
