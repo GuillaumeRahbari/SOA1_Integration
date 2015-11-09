2f886a1eae0469f97441e6ca8f7d0f6745689eca
Marc Karassev <marc.karassev@greencom-networks.com>
2015-11-09 15:54:21 +0100

    màj script

--- build_and_deploy_shop3000.sh
+++ build_and_deploy_shop3000.sh
@@ -2,4 +2,4 @@ echo "assuming servicemix is already running and that you have an apache-service
 echo "also, this script must be run at the SOA1_Integration root directory"
 
 mvn clean package -rf :Shop3000
-cp Shop3000/target/Shop3000-1.0-SNAPSHOT.jar ~/apache-servicemix-6.0.0/deploy/
+cp Shop3000/target/Shop3000-1.0-SNAPSHOT.jar ~/Applications/apache-servicemix-6.0.0/deploy/
