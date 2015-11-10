8aaf9af4c54cd12d45506c8e3c76e214878b37a2
Quentin cornevin <qcornevin@gmail.com>
2015-11-10 10:10:00 +0100

    Je refractor 5 minutes après le push ..

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/delivery/DeliveryFlow.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/delivery/DeliveryFlow.java
@@ -1,8 +1,14 @@
 package fr.unice.polytech.soa1.shop3000.flows.delivery;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.unice.polytech.soa1.shop3000.business.PaymentInformation;
 import fr.unice.polytech.soa1.shop3000.flows.pay.PayEndpoint;
+import fr.unice.polytech.soa1.shop3000.flows.pay.PayUnmarshaller;
 import fr.unice.polytech.soa1.shop3000.flows.pay.ProceedPayment;
+import fr.unice.polytech.soa1.shop3000.utils.MockDeliverySystem;
+import fr.unice.polytech.soa1.shop3000.utils.Shop3000Information;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
@@ -10,23 +16,41 @@ import org.apache.camel.builder.RouteBuilder;
  */
 public class DeliveryFlow extends RouteBuilder {
 
-    private MockedDeliverySystem mockedDeliverySystem = new MockedDeliverySystem();
+    public static String DELIVERY_PRICE = "deliveryPrice";
 
+    private HandleDeliveryPrice handleDeliveryPrice = new HandleDeliveryPrice();
 
     @Override
     public void configure() throws Exception {
 
         from(DeliveryEndpoints.GET_DELIVERY_PRICE.getInstruction())
                 .log("Begin of the flow to get the delivery Price")
-                .log("${property.paymentInformation}")
 
-                        /** {@link MockedDeliverySystem#process(Exchange)}  **/
-                .process(mockedDeliverySystem)
-                .log("${property.deliveryPrice}")
+                        /** {@link HandleDeliveryPrice#process(Exchange)}  **/
+                .process(handleDeliveryPrice)
 
                 /** {@link ProceedPayment#configure()}  **/
                 .to(PayEndpoint.SHOP3000_PAYMENT.getInstruction())
         ;
 
+    }
+
+
+    /**
+     * This process will add a property where the name is defined in the constant DELIVERY_PRICE
+     * This property will contain the price of the delivery given by the the delivery system.
+     */
+    private class HandleDeliveryPrice implements Processor {
+
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            ObjectMapper objectMapper = new ObjectMapper();
+            PaymentInformation paymentInformation =  objectMapper.readValue((String) exchange.getProperty(PayUnmarshaller.PAYMENT_INFORMATION_PROPERTY), PaymentInformation.class);
+            String clientAddress = paymentInformation.getAddress();
+
+            int price = MockDeliverySystem.getDeliveryPrice(Shop3000Information.ADDRESS, clientAddress);
+            exchange.setProperty(DELIVERY_PRICE, price);
+
+        }
     }
 }
---
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/utils/MockDeliverySystem.java
@@ -0,0 +1,19 @@
+package fr.unice.polytech.soa1.shop3000.utils;
+
+/**
+ * Created by Quentin on 11/10/2015.
+ */
+public class MockDeliverySystem {
+
+    /**
+     * This is the best mock ever.
+     * Seriously this method mock the call of a partner who will give us the price of a delivery
+     * for two given address
+     * @param startAddress address of shop3000
+     * @param endAddress address of the client
+     * @return the price of the devliery
+     */
+    public static int getDeliveryPrice(String startAddress, String endAddress) {
+        return 666; // #Sataniste
+    }
+}
