83720d2d9d2902562c82d8f54b93a186f0dd423c
Marc Karassev <marc.karassev@greencom-networks.com>
2015-11-11 18:42:01 +0100

    total fail

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/PayShop.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/PayShop.java
@@ -1,6 +1,7 @@
 package fr.unice.polytech.soa1.shop3000.flows.pay;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.unice.polytech.soa1.shop3000.business.payment.BikoPaymentInformation;
 import fr.unice.polytech.soa1.shop3000.business.payment.PaymentInformation;
 import fr.unice.polytech.soa1.shop3000.business.payment.VolleyPaymentInformation;
 import fr.unice.polytech.soa1.shop3000.flows.pay.defs.PayEndpoint;
@@ -16,10 +17,11 @@ import org.apache.camel.builder.RouteBuilder;
 public class PayShop extends RouteBuilder {
 
     private static final String BEER_PAYMENT_SERVICE_ADDRESS = "",
-        BIKO_PAYMENT_SERVICE_ADDRESS = "",
+        BIKO_CART_SERVICE_ADDRESS = "http://localhost:8181/cxf/biko/user/",
         VOLLEY_PAYMENT_SERVICE_ADDRESS = "http://localhost:8181/cxf/volley/payments/"; // needs account id path param
 
     private MakeVolleyPaymentInformation makeVolleyPaymentInformation = new MakeVolleyPaymentInformation();
+    private MakeBikoPaymentInformation makeBikoPaymentInformation = new MakeBikoPaymentInformation();
 
     @Override
     public void configure() throws Exception {
@@ -34,7 +36,12 @@ public class PayShop extends RouteBuilder {
          * Flow proceeding to Biko payment.
          */
         from(PayEndpoint.PAY_BIKO.getInstruction()).log("lol")
-            ; // TODO
+            .log("paying Biko")
+            .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+            .setHeader(Exchange.HTTP_PATH, simple("${header." + Exchange.HTTP_PATH + "}/cart"))
+            .process(makeBikoPaymentInformation)
+            .to(BIKO_CART_SERVICE_ADDRESS)
+            .setBody(constant(true)); // TODO may fail
 
         /**
          * Flow proceeding to Volley on the Beach payment.
@@ -44,24 +51,56 @@ public class PayShop extends RouteBuilder {
                 .setHeader(Exchange.HTTP_METHOD, constant("POST"))
                 .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
                 .process(makeVolleyPaymentInformation)
-                .to(VOLLEY_PAYMENT_SERVICE_ADDRESS); // TODO
+                .to(VOLLEY_PAYMENT_SERVICE_ADDRESS)
+                .setBody(constant(true)); // TODO may fail...
+    }
+
+    /**
+     * Abstract class for processors which roles are to transform shop3000 payment information objects into shop
+     * specific payment information objects.
+     * Expects the exchange body to represent a PaymentInformation object.
+     */
+    private abstract class MakePaymentInformation extends SuperProcessor {
+
+        // Attributes
+
+        protected ObjectMapper mapper = new ObjectMapper();
+        protected PaymentInformation information;
+
+        // Methods
+
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            information = mapper.readValue((String) exchange.getIn().getBody(), PaymentInformation.class);
+            transformPaymentInformation(exchange);
+        }
+
+        protected abstract void transformPaymentInformation(Exchange exchange) throws Exception;
     }
 
     /**
      * Transforms a shop3000 payment information object into a Volley on the Beach payment information object.
      * Expects the exchange body to represent a PaymentInformation object. Resets it to the resulting value.
      */
-    private class MakeVolleyPaymentInformation extends SuperProcessor {
+    private class MakeVolleyPaymentInformation extends MakePaymentInformation {
 
         @Override
-        public void process(Exchange exchange) throws Exception {
-            ObjectMapper objectMapper = new ObjectMapper();
-            PaymentInformation paymentInformation = objectMapper.readValue((String) exchange.getIn().getBody(),
-                    PaymentInformation.class);
-            VolleyPaymentInformation volleyPaymentInformation = new VolleyPaymentInformation(paymentInformation);
+        protected void transformPaymentInformation(Exchange exchange) throws Exception {
+            VolleyPaymentInformation volleyPaymentInformation = new VolleyPaymentInformation(information);
+            exchange.getIn().setBody(mapper.writeValueAsString(volleyPaymentInformation));
+        }
+    }
 
-            exchange.getIn().setBody(objectMapper.writeValueAsString(volleyPaymentInformation));
-            System.out.println(exchange.getIn().getBody());
+    /**
+     * Transforms a shop3000 payment information object into a Biko payment information object.
+     * Expects the exchange body to represent a PaymentInformation object. Resets it to the resulting value.
+     */
+    private class MakeBikoPaymentInformation extends MakePaymentInformation {
+
+        @Override
+        protected void transformPaymentInformation(Exchange exchange) throws Exception {
+            BikoPaymentInformation bikoPaymentInformation = new BikoPaymentInformation(information);
+            exchange.getIn().setBody(mapper.writeValueAsString(bikoPaymentInformation));
         }
     }
 }
