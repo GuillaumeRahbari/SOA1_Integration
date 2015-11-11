af482192d651513a792ef4471bc191f0f5e8da9c
Rahbari Guillaume <guillaume.rahbari@gmail.com>
2015-11-11 13:13:55 +0100

    All we need to do correctly the best seller

---
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/flows/pay/BestSellerBean.java
@@ -0,0 +1,46 @@
+package fr.unice.polytech.soa1.shop3000.flows.pay;
+
+import fr.unice.polytech.soa1.shop3000.business.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by guillaume on 10/11/2015.
+ */
+public class BestSellerBean {
+
+    /**
+     * This method updates the best seller.
+     *
+     * @param clientFirstName The client's first name.
+     */
+    public void updateBestSeller (String clientFirstName){
+        // We get the client
+        Client client = ClientStorage.read(clientFirstName);
+        // We get the client's cart
+        Cart clientCart = client.getCart();
+        // We create an iterator over the client's cart hashmap
+        Iterator it = clientCart.entrySet().iterator();
+        while (it.hasNext()){
+            // We get the pair (key, value)
+            Map.Entry pair = (Map.Entry)it.next();
+            // On parcourt la partie value qui est une arraylist
+            for (CatalogItem catalogItem : (ArrayList<CatalogItem>)pair.getValue()) {
+
+                // If it exists in the list.
+                if (BestSellerList.getBestSellerList().containsKey(catalogItem)) {
+                    BestSellerList.getBestSellerList().put(catalogItem, BestSellerList.getBestSellerList().get(catalogItem) + 1);
+                }
+                // If not
+                else {
+                    BestSellerList.addItemToList(catalogItem);
+                }
+            }
+            it.remove(); // avoids a ConcurrentModificationException
+        }
+    }
+
+}
