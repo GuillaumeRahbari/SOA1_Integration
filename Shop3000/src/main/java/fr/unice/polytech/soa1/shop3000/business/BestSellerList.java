b33222192e546aef36529c73ce300844d9bcf6c7
Marc Karassev <marc.karassev@yahoo.fr>
2015-11-15 12:46:11 +0100

    static init best seller

--- Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/business/BestSellerList.java
+++ Shop3000/src/main/java/fr/unice/polytech/soa1/shop3000/business/BestSellerList.java
@@ -1,30 +1,29 @@
 package fr.unice.polytech.soa1.shop3000.business;
 
 import fr.unice.polytech.soa1.shop3000.business.catalog.CatalogItem;
+import fr.unice.polytech.soa1.shop3000.business.catalog.ItemDescription;
 
 import java.util.HashMap;
 
 /**
- * Created by Quentin on 11/9/2015.
+ * Created by Quentin on 11/9/2015. {"name":"net","price":9.5,"description":{"color":"BLUE"}}
  */
 public class BestSellerList {
 
-    private static HashMap<CatalogItem, Integer> bestSellerList = new HashMap<>();
+    private static final CatalogItem INITIAL_BEST_SELLER = new CatalogItem("net", 9.5, new ItemDescription("BLUE"), null);
+
+    private static HashMap<CatalogItem, Integer> bestSellerList;
 
     public static HashMap<CatalogItem, Integer> getBestSellerList() {
         return bestSellerList;
-    }
-
-    public CatalogItem getBestSeller() {
-        return null;
     }
 
     public static void addItemToList (CatalogItem catalogItem) {
         bestSellerList.put(catalogItem, 1);
     }
 
-
-    public void setBestSellerList(HashMap<CatalogItem, Integer> bestSellerList) {
-        this.bestSellerList = bestSellerList;
+    static {
+        bestSellerList = new HashMap<>();
+        addItemToList(INITIAL_BEST_SELLER);
     }
 }
